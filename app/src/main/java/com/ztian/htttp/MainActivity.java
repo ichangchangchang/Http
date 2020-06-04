dbbc7745a7a5efff78e8fe550dc31012a8ea3df8
ichangchangchang <1035190820@qq.com>
2020-06-04 23:24:51 +0800

    firstgit

--- app/src/main/java/com/ztian/htttp/MainActivity.java
+++ app/src/main/java/com/ztian/htttp/MainActivity.java
@@ -19,7 +19,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-
+//22222222222222222222222222222222222222222222222222222
 public class MainActivity extends AppCompatActivity {
      TextView tv;
     Button btn;
